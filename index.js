408918f78b62d9e3ada18b9b28ada35551ab679b
IngridGdesigns <garciai@protonmail.com>
2024-01-02 14:42:06 -0800

    renamed variables and refactored functions, found bug need to look further

--- index.js
+++ index.js
@@ -1,44 +1,51 @@
-let homeCount = 0;
-let guestCount = 0;
+let homeScore = 0;
+let guestScore = 0;
 
 let home = document.getElementById("home");
 let guest = document.getElementById("guest")
 
-function increment(num, team) {
-        if(team === home){
-            if(num === 1){
-            homeCount = homeCount + 1;
-            home.innerText = homeCount;
-        } else if(num === 2) {
-            homeCount = homeCount + 2;
-            home.innerText = homeCount;
-        } else {
-            homeCount = homeCount + 3;
-            home.innerText = homeCount;
-        }
+function increment1(team) {
+    if (team === "home") {
+        homeScore += 1;
+        home.textContent = homeScore;
+    } else if (team === "guest") {
+        guestScore += 1;
+        guest.textContent = guestScore;
     }
+}
 
-        if(team === guest){
-            if(num === 1){
-            guestCount = guestCount + 1;
-            guest.innerText = guestCount;
-        } else if(num === 2) {
-            guestCount = guestCount + 2;
-            guest.innerText = guestCount;
-        } else {
-            guestCount = guestCount + 3;
-            guest.innerText = guestCount;
-        }
+function increment2(team) {
+    if (team === "home") {
+        homeScore += 2;
+        home.textContent = homeScore;
+    } else if (team === "guest") {
+        guestScore += 2;
+        guest.textContent = guestScore;
     }
-   
+}
+
+function increment3(team) {
+    if (team === "home") {
+        homeScore += 3;
+        home.textContent = homeScore;
+    } else if (team === "guest") {
+        guestScore += 3;
+        guest.textContent = guestScore;
+    }
 }
 
 let reset = document.getElementById("newGame");
 
 function newGame() {
-    home.innerText = 0;
-    guest.innerText = 0;
+    home.textContent = 0;
+    guest.textContent = 0;
 }
+
+
+// function start() {
+//     home.textContent = 0;
+//     guest.textContent = 0;
+// }
 
 // let isLeading = document.getElementById("leader");
 
@@ -48,9 +55,17 @@ function newGame() {
 
 // isLeading.innerHTML
 // // function isleading () {
-// //     if(homeCount > guestCount){
+// //     if(homeScore > guestScore){
 // //         console.log("highlight leader");
-// //     } else if(guestCount > homeCount){
+// //     } else if(guestScore > homeScore){
 // //         console.log("highlight guest");
 // //     }
 // // }
+
+// Stopwatch https://javacodepoint.com/how-to-create-a-stopwatch-in-javascript/
+// let stopwatch = document.getElementById("stopwatch");
+// let startBtn = document.getElementById("start-btn");
+// let timeoutId = null;
+// let ms = 0;
+// let sec = 0;
+// let min = 0;
