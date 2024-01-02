dd315b4942677d37d9a7e6418cc20dacd7c744ad
IngridGdesigns <garciai@protonmail.com>
2024-01-02 14:53:04 -0800

    edited names for clarification

--- index.js
+++ index.js
@@ -1,42 +1,43 @@
 let homeScore = 0;
 let guestScore = 0;
 
-let home = document.getElementById("home");
-let guest = document.getElementById("guest")
+let homeEl = document.getElementById("home-el");
+let guestEl = document.getElementById("guest-el")
 
 function increment1(team) {
     if (team === "home") {
         homeScore += 1;
-        home.textContent = homeScore;
+        homeEl.textContent = homeScore;
     } else if (team === "guest") {
         guestScore += 1;
-        guest.textContent = guestScore;
+        guestEl.textContent = guestScore;
     }
 }
 
 function increment2(team) {
     if (team === "home") {
         homeScore += 2;
-        home.textContent = homeScore;
+        homeEl.textContent = homeScore;
     } else if (team === "guest") {
         guestScore += 2;
-        guest.textContent = guestScore;
+        guestEl.textContent = guestScore;
     }
 }
 
 function increment3(team) {
     if (team === "home") {
         homeScore += 3;
-        home.textContent = homeScore;
+        homeEl.textContent = homeScore;
     } else if (team === "guest") {
         guestScore += 3;
-        guest.textContent = guestScore;
+        guestEl.textContent = guestScore;
     }
 }
 
 let reset = document.getElementById("new");
 
 function newGame() {
-    home.textContent = 0;
-    guest.textContent = 0;
+    homeEl.textContent = 0;
+    guestEl.textContent = 0;
+
 }
